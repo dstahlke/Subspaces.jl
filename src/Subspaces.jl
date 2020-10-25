45fbfa192e186c827942087927589c5ea2708dad
Dan Stahlke <dan@stahlke.org>
2020-10-24 17:58:41 -0700

    misc

--- src/Subspaces.jl
+++ src/Subspaces.jl
@@ -7,6 +7,7 @@ using LinearAlgebra
 export Subspace, shape, dim, each_basis_element
 export random_subspace, random_hermitian_subspace, empty_subspace, full_subspace
 export tobasis, frombasis, random_element, projection, perp
+export hermitian_basis
 
 struct Subspace{T, N}
     basis::AbstractArray
@@ -70,8 +71,18 @@ end
 *(a::Subspace, b::Subspace) =
     Subspace([ x*y for x in each_basis_element(a) for y in each_basis_element(b) ])
 
-kron(a::Subspace, b::Subspace) =
-    Subspace([ kron(x, y) for x in each_basis_element(a) for y in each_basis_element(b) ])
+function kron(a::Subspace, b::Subspace)
+    if dim(a) == 0 || dim(b) == 0
+        return Subspace([
+            kron(zeros(shape(a)), zeros(shape(b))) ])
+    else
+        return Subspace([
+            kron(x, y)
+            for x in each_basis_element(a)
+            for y in each_basis_element(b)
+        ])
+    end
+end
 
 vcat(ss::Subspace...) = cat(ss...; dims=1)
 
@@ -98,6 +109,14 @@ end
 adjoint(ss::Subspace{<:Number, 2}) =
     Subspace([ x' for x in each_basis_element(ss) ])
 
+function in(x::UniformScaling, ss::Subspace{T, 2}) where T
+    return Matrix{T}(I, shape(ss)) in ss
+end
+
+function in(x::AbstractArray{<:Number, N}, ss::Subspace{<:Number, N}) where N
+    return norm(x - projection(ss, x)) <= ss.tol
+end
+
 function in(a::Subspace{<:Number, N}, b::Subspace{<:Number, N}) where N
     shp = shape(a)
     if dim(a) > dim(b)
@@ -116,6 +135,8 @@ end
 
 (+)(a::Subspace, b::AbstractArray) = a + Subspace([b])
 (+)(a::AbstractArray, b::Subspace) = Subspace([a]) + b
+(+)(ss::Subspace{T, 2}, x::UniformScaling) where T = ss + Subspace([ Array{T}(I, shape(ss)) ])
+(+)(x::UniformScaling, ss::Subspace{T, 2}) where T = ss + I
 
 (*)(a::Subspace, b::AbstractArray) = a * Subspace([b])
 (*)(a::AbstractArray, b::Subspace) = Subspace([a]) * b
@@ -160,16 +181,28 @@ random_element(ss::Subspace{T}) where T <: Number = frombasis(ss, randn(T, dim(s
 # FIXME take datatype parameter
 # FIXME should use a unitarily invariant distribution
 function random_subspace(d::Int, dims)
-    b = [ randn(ComplexF64, dims) for i in 1:d ]
-    return Subspace(b)
+    if d < 0
+        throw(ArgumentError("subspace dimension was negative: $d"))
+    elseif d == 0
+        return empty_subspace(dims)
+    else
+        b = [ randn(ComplexF64, dims) for i in 1:d ]
+        return Subspace(b)
+    end
 end
 
 # FIXME take datatype parameter
 # FIXME should use a unitarily invariant distribution
 function random_hermitian_subspace(d::Int, n::Int)
-    b = [ randn(ComplexF64, n, n) for i in 1:d ]
-    b = [ x + x' for x in b ]
-    return Subspace(b)
+    if d < 0
+        throw(ArgumentError("subspace dimension was negative: $d"))
+    elseif d == 0
+        return empty_subspace((n, n))
+    else
+        b = [ randn(ComplexF64, n, n) for i in 1:d ]
+        b = [ x + x' for x in b ]
+        return Subspace(b)
+    end
 end
 
 # FIXME take datatype parameter
@@ -177,5 +210,50 @@ empty_subspace(dims::Tuple) = Subspace([zeros(dims)])
 
 # FIXME take datatype parameter
 full_subspace(dims::Tuple) = perp(empty_subspace(dims))
+
+#############
+### Hermitian
+#############
+
+function hermit_to_vec(M::AbstractArray{Complex{T}, 2}) where T
+    n = size(M)[1]
+    size(M)[2] == n || throw(ArgumentError("matrix was not square: $(size(M))"))
+    v = zeros(T, n*n)
+    k = 0
+    for i in 1:n
+        v[k+1:k+i] = real(M[1:i, i])
+        k += i
+        v[k+1:k+i-1] = imag(M[1:i-1, i])
+        k += i-1
+    end
+    return v
+end
+
+function vec_to_hermit(v::AbstractArray{T, 1}, n::Integer) where T
+    @assert n*n == size(v)[1]
+    M = zeros(Complex{T}, n, n)
+    k = 0
+    for i in 1:n
+        M[1:i, i] = v[k+1:k+i]
+        M[i, 1:i] = v[k+1:k+i]
+        k += i
+        M[1:i-1, i] += v[k+1:k+i-1] * 1im
+        M[i, 1:i-1] -= v[k+1:k+i-1] * 1im
+        k += i-1
+    end
+    return Hermitian(M)
+end
+
+function hermitian_basis(ss::Subspace{Complex{T}}) where T
+    n = shape(ss)[1]
+    shape(ss)[2] == n || throw(ArgumentError("subspace shape was not square: $(shape(ss))"))
+    M = hcat(
+        [ hermit_to_vec( x     + x'     ) for x in each_basis_element(ss) ]...,
+        [ hermit_to_vec((x*1im)+(x*1im)') for x in each_basis_element(ss) ]...
+    )
+    hb = [ vec_to_hermit(x, n) for x in each_basis_element(Subspace(M)) ]
+    @assert (Subspace(hb) == ss) "Hermitian basis didn't equal original space"
+    return hb
+end
 
 end # module
